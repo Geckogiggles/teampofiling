105b0c2b8434a8a00ce52f6c9e11b17fe1632bb3
GeckoGiggles <eva.judith.perez@gmail.com>
2023-02-08 16:24:46 -0500

    added and rearranged tailwind elements within the profile.js directory

--- develop/index.js
+++ develop/index.js
@@ -1,37 +1,43 @@
-const fs = require ('fs');
-const generateProfiles = require ('./profile');
-const inquirer = require ('inquirer');
+const fs = require('fs');
+const generateProfiles = require('./profile');
+const inquirer = require('inquirer');
+const Employee = require('../lib/Employee');
+const Manager = require ('../lib/Manager');
+const Engineer = require ('../lib/Engineer');
+const Intern = require ('../lib/Intern');
+    const generateHTML = require('generateHTML');
 const { writeFile } = require('fs').promises;
-const promptUser = () => {
-    return inquirer.prompt([
-        {
-            type:'input',
-            name:'teamName',
-            message: 'Tell me your team name!',
-            validate:(value) => {
-                if (value) {
-                    return true;
+const teamArray = []
+    function promptUserforManager() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'teamName',
+                message: 'Tell me your team name!',
+                validate: (value) => {
+                    if (value) {
+                        return true;
                     } else {
                         console.log("Please choose a team name and type it in to continue your 'Team Info' page.")
                     }
                 }
-        },
-        {
-            type:'input',
-            name: 'name',
-            message: "What is your team manager's name?",
-            validate: (value) => {
-                if (value) {
-                    return true;
-                } else {
-                    console.log("Please enter the project manager's name to continue.")
-                }
-            }
-        },
+            },
             {
-                type:'input',
-                name:'employeeid',
-                message:'Please insert employee id',
+                type: 'input',
+                name: 'name',
+                message: "What is your team manager's name?",
+                validate: (value) => {
+                    if (value) {
+                        return true;
+                    } else {
+                        console.log("Please enter the project manager's name to continue.")
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'employeeid',
+                message: 'Please insert employee id',
                 validate: (value) => {
                     if (value) {
                         return true;
@@ -41,8 +47,8 @@ const promptUser = () => {
                 }
             },
             {
-                type:'input',
-                name:'email',
+                type: 'input',
+                name: 'email',
                 message: "Please provide the project manager's email address.",
                 validate: (value) => {
                     if (value) {
@@ -50,11 +56,11 @@ const promptUser = () => {
                     } else {
                         console.log("Please provide the project manager's email address to continue.")
                     }
-                    }
+                }
             },
             {
-                type:'input',
-                name:'officeNumber',
+                type: 'input',
+                name: 'officeNumber',
                 message: "Please provide the project office's phone number.",
                 validate: (value) => {
                     if (value) {
@@ -62,9 +68,202 @@ const promptUser = () => {
                     } else {
                         console.log("Please provide the project office's phone number to continue.")
                     }
+                }
+            },
+
+        ])
+            .then((answers) => {
+                const employee = new Manager(answers.name, answers.employeeid, answers.email, answers.officeNumber)
+                teamArray.push(employee)
+                addTeamMember()
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+    //functions that will build those respective parts of html and prompts
+    function addTeamMember() {
+        inquirer.prompt([
+            {
+                type: 'expand',
+                name: 'employee',
+                message: "Would you like to add another employee to your team?",
+                choices: [
+                    {
+                        key: 'n',
+                        name: 'No-more-employees',
+                        value: 'alldone',
+                    },
+                    {
+                        key: 'i',
+                        name: 'Intern',
+                        value: 'intern',
+                    },
+                    {
+                        key: 'e',
+                        name: 'Engineer',
+                        value: 'engineer',
                     }
-            }
-        ]);
+                ]
+                    .then((answers) => {
+                        if (answers.key === 'n') {
+                            return writetoFile();
+                        } else if (answers.key === 'i') {
+                            return addIntern()
+                        } else {
+                            return addEngineer()
+                        }
+                    }
+                    )
+            }])
+    }
+    function addIntern() {
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'teamName',
+                    message: 'Tell me your team name!',
+                    validate: (value) => {
+                        if (value) {
+                            return true;
+                        } else {
+                            console.log("Please choose a team name and type it in to continue your 'Team Info' page.")
+                        }
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'name',
+                    message: "What is your team manager's name?",
+                    validate: (value) => {
+                        if (value) {
+                            return true;
+                        } else {
+                            console.log("Please enter the project manager's name to continue.")
+                        }
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'employeeid',
+                    message: 'Please insert employee id',
+                    validate: (value) => {
+                        if (value) {
+                            return true;
+                        } else {
+                            console.log('Please provide employee ID to continue.')
+                        }
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'email',
+                    message: "Please provide the project manager's email address.",
+                    validate: (value) => {
+                        if (value) {
+                            return true;
+                        } else {
+                            console.log("Please provide the project manager's email address to continue.")
+                        }
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'School',
+                    message: "Please provide name of your school.",
+                    validate: (value) => {
+                        if (value) {
+                            return true;
+                        } else {
+                            console.log("Please provide the the name of your school to continue.")
+                        }
+                    }
+                },
+    
+            ])
+                .then((answers) => {
+                const employee = new Intern(answers.name, answers.employeeid, answers.email, answers.School)
+                teamArray.push(employee)
+                    addTeamMember()
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+    }
+    function addEngineer() {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'teamName',
+                message: 'Tell me your team name!',
+                validate: (value) => {
+                    if (value) {
+                        return true;
+                    } else {
+                        console.log("Please choose a team name and type it in to continue your 'Team Info' page.")
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is your team manager's name?",
+                validate: (value) => {
+                    if (value) {
+                        return true;
+                    } else {
+                        console.log("Please enter the project manager's name to continue.")
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'employeeid',
+                message: 'Please insert employee id',
+                validate: (value) => {
+                    if (value) {
+                        return true;
+                    } else {
+                        console.log('Please provide employee ID to continue.')
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Please provide the project manager's email address.",
+                validate: (value) => {
+                    if (value) {
+                        return true;
+                    } else {
+                        console.log("Please provide the project manager's email address to continue.")
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'gitHub',
+                message: "Please provide your GitHub username.",
+                validate: (value) => {
+                    if (value) {
+                        return true;
+                    } else {
+                        console.log("Please provide your GitHub username to continue.")
+                    }
+                }
+            },
+
+        ])
+            .then((answers) => {
+                const employee = new Engineer(answers.name, answers.employeeid, answers.email, answers.gitHub)
+                teamArray.push(employee)
+                addTeamMember()
+            })
+            .catch((error) => {
+                console.log(error)
+            })
 
     }
-
+    //import generateHTML and then call it and give it teamArray and whatever it returns you pass it into the write file.
+    writeFile('./develop', generateHTML(teamArray))
+promptUserforManager();
--- develop/profile.js
+++ develop/profile.js
@@ -1,6 +1,95 @@
 const teamInformation = require('./index')
-//tailwind card element here
-const generateProfiles = `<!DOCTYPE html>
+//tailwind card element here:
+function generateHTML(teamArray){
+  const htmlCode = ''
+  teamArray.forEach(employee => {
+  if (employee.getRole() === "Manager") {
+    const generateManagerProfile = `
+    <div class="max-w-sm w-full lg:max-w-full lg:flex">
+      <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('/img/card-left.jpg')" title="Woman holding a mug">
+      </div>
+      <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div class="mb-8">
+          <p class="text-sm text-gray-600 flex items-center">
+            <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
+            </svg>
+            Employees only
+          </p>
+          <div class="text-gray-900 font-bold text-xl mb-2">${employee.getRole()}</div>
+          <p class="text-gray-700 text-base">${employee.name}</p>
+        </div>
+        <div class="flex items-center">
+          <img class="w-10 h-10 rounded-full mr-4" src="https://pixabay.com/get/g65817b1e62dc10b03a647a96e20f22f2340fc1625dd9b5fd4c42952fb8645a250c46cc7d3f39433a31f5063a5dd98ec7_1280.jpg" alt="random stock image">
+          <div class="text-sm">
+            <p class="text-gray-900 leading-none">${employee.officeNumber}</p>
+            <p class="text-gray-600">${employee.email}</p>
+          </div>
+        </div>
+      </div>
+    </div> 
+    `;
+    teamCard += generateManagerProfile
+  } else if (employee.getRole() === "Engineer") {
+
+    const generateEngineerProfile = `
+    <div class="max-w-sm w-full lg:max-w-full lg:flex">
+    <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('/img/card-left.jpg')" title="Woman holding a mug">
+    </div>
+    <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+      <div class="mb-8">
+        <p class="text-sm text-gray-600 flex items-center">
+          <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
+          </svg>
+          Employees only
+        </p>
+        <div class="text-gray-900 font-bold text-xl mb-2">${employee.getRole()}</div>
+        <p class="text-gray-700 text-base">${employee.name}</p>
+      </div>
+      <div class="flex items-center">
+        <img class="w-10 h-10 rounded-full mr-4" src="https://pixabay.com/get/g65817b1e62dc10b03a647a96e20f22f2340fc1625dd9b5fd4c42952fb8645a250c46cc7d3f39433a31f5063a5dd98ec7_1280.jpg" alt="random stock image">
+        <div class="text-sm">
+          <p class="text-gray-900 leading-none">${employee.gitHub}</p>
+          <p class="text-gray-600">${employee.email}</p>
+        </div>
+      </div>
+    </div>
+    </div> 
+    `;
+    teamCard += generateEngineerProfile
+  } else if(employee.getRole() === "Intern"){
+
+    const generateInternProfile = `
+    <div class="max-w-sm w-full lg:max-w-full lg:flex">
+    <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('/img/card-left.jpg')" title="Woman holding a mug">
+    </div>
+    <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+      <div class="mb-8">
+        <p class="text-sm text-gray-600 flex items-center">
+          <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
+          </svg>
+          Employees only
+        </p>
+        <div class="text-gray-900 font-bold text-xl mb-2">${employee.getRole()}</div>
+        <p class="text-gray-700 text-base">${employee.name}</p>
+      </div>
+      <div class="flex items-center">
+        <img class="w-10 h-10 rounded-full mr-4" src="https://pixabay.com/get/g65817b1e62dc10b03a647a96e20f22f2340fc1625dd9b5fd4c42952fb8645a250c46cc7d3f39433a31f5063a5dd98ec7_1280.jpg" alt="random stock image">
+        <div class="text-sm">
+          <p class="text-gray-900 leading-none">${employee.School}</p>
+          <p class="text-gray-600">${employee.email}</p>
+        </div>
+      </div>
+    </div>
+    </div> 
+    `;
+    teamCard += generateInternProfile
+  }
+  });
+
+return `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,30 +99,9 @@ const generateProfiles = `<!DOCTYPE html>
     <title>Team Information- ${teamName}</title>
 </head>
 <body>
-<div class="max-w-sm w-full lg:max-w-full lg:flex">
-  <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('/img/card-left.jpg')" title="Woman holding a mug">
-  </div>
-  <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-    <div class="mb-8">
-      <p class="text-sm text-gray-600 flex items-center">
-        <svg class="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-          <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
-        </svg>
-        Employees only
-      </p>
-      <div class="text-gray-900 font-bold text-xl mb-2">${name}</div>
-      <p class="text-gray-700 text-base">${name}</p>
-    </div>
-    <div class="flex items-center">
-      <img class="w-10 h-10 rounded-full mr-4" src="https://pixabay.com/get/g65817b1e62dc10b03a647a96e20f22f2340fc1625dd9b5fd4c42952fb8645a250c46cc7d3f39433a31f5063a5dd98ec7_1280.jpg" alt="random stock image">
-      <div class="text-sm">
-        <p class="text-gray-900 leading-none">Jonathan Reinink</p>
-        <p class="text-gray-600">Aug 18</p>
-      </div>
-    </div>
-  </div>
-</div> 
+${teamCard}
 </body>
-</html>
-`;
-module.exports= generateProfiles
+</html>`
+}
+
+module.exports= generateHTML
